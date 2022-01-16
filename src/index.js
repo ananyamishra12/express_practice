972093c75290cbaf5e8ca806cbd2be407751ec73
Ananya Mishra <66728375+ananyamishra12@users.noreply.github.com>
2022-01-17 02:41:52 +0530

    hbs

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app= express();
 const port=8000;
 const staticPath = path.join(__dirname, "../public");
-app.use(express.static(staticPath));
 
+app.set("view engine", "hbs");
+//app.use(express.static(staticPath));
+
+app.get("", (req, res)=>{
+    res.render("index", {
+        channelName: "ananya",
+    });
+})
 //app.get(route, callback)
 app.get("/" , (req, res)=>{
     res.send("hello from the express");
